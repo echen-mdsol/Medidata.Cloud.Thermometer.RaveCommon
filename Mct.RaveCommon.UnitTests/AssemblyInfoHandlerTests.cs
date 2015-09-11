8900cb3816bc76ef457df80349bd22245ac611d4
Eric Chen <echen@mdsol.com>
2015-09-11 12:45:57 -0400

    Added AssemblyInfoHandler

---
+++ Mct.RaveCommon.UnitTests/AssemblyInfoHandlerTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.AutoRhinoMock;
+
+namespace Medidata.Cloud.Thermometer.RaveCommon.UnitTests
+{
+    [TestClass]
+    public class AssemblyInfoHandlerTests
+    {
+        private IFixture _fixture;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _fixture = new Fixture().Customize(new AutoRhinoMockCustomization());
+        }
+
+        [TestMethod]
+        public void HandlerShouldNotReturnNull()
+        {
+            //Arrange
+            var question = _fixture.Create<IThermometerQuestion>();
+
+            //Act
+            var sut = new AssemblyInfoHandler("/abc");
+            var answer = sut.Handler(question);
+
+            //Assert
+            Assert.IsNotNull(answer);
+        }
+    }
+}
---
+++ Mct.RaveCommon/AssemblyInfoHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Medidata.Cloud.Thermometer.RaveCommon
+{
+    public class AssemblyInfoHandler : ThermometerBaseHandler
+    {
+        public AssemblyInfoHandler(string route, string name = null)
+            : base(route, name)
+        {
+          
+        }
+
+        protected override object HandleQuestion(IThermometerQuestion question)
+        {
+            var assemblyNames = AppDomain.CurrentDomain.GetAssemblies().Select(a => a.GetName());
+
+            return assemblyNames.Where(an => an.Name.Contains("Medidata"))
+                .ToDictionary(an => an.Name, an => an.Version.ToString());
+        }
+    }
+}
