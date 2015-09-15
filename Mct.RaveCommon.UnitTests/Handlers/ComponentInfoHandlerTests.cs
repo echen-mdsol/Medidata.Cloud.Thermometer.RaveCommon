00db54a558b407d69c7d4679b5a4e81af534e8f5
Eric Chen <echen@mdsol.com>
2015-09-15 11:43:03 -0400

    ComponentInfoHandler functionality

---
+++ Mct.RaveCommon.UnitTests/Handlers/ComponentInfoHandlerTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Medidata.Cloud.Thermometer.RaveCommon.Handlers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.AutoRhinoMock;
+
+namespace Medidata.Cloud.Thermometer.RaveCommon.UnitTests.Handlers
+{
+    [TestClass]
+    public class ComponentInfoHandlerTests
+    {
+        private IFixture _fixture;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _fixture = new Fixture().Customize(new AutoRhinoMockCustomization());
+        }
+
+        [TestMethod]
+        public void HandlerShouldNotReturnNull()
+        {
+            var question = _fixture.Create<IThermometerQuestion>();
+
+            var sut = new ComponentInfoHandler();
+            var answer = sut.Handler(question);
+
+            Assert.IsNotNull(answer);
+        }
+    }
+}
--- Mct.RaveCommon/Handlers/ComponentInfoHandler.cs
+++ Mct.RaveCommon/Handlers/ComponentInfoHandler.cs
@@ -1,11 +1,58 @@
-﻿namespace Medidata.Cloud.Thermometer.RaveCommon.Handlers
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Medidata.Cloud.Thermometer.RaveCommon.Handlers
 {
     public class ComponentInfoHandler : ThermometerBaseHandler
     {
+        private readonly List<string> raveComponentNames = new List<string>()
+        {
+            "Medidata.Core.Service",
+            "Medidata.Rave.Integration.Service",
+            "Medidata.RaveWebServices.Web",
+            "MedidataRAVE",
+            "RaveCrystalViewer"
+        };
+
+        private Assembly GetComponentAssembly()
+        {
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+
+            foreach (var assembly in assemblies)
+            {
+                if (raveComponentNames.Contains(assembly.GetName().Name))
+                {
+                    return assembly;
+                }
+            }
+
+            return assemblies.First();
+        }
+
+        public T GetAssemblyAttribute<T>(Assembly assembly) where T : Attribute
+        {
+            var attributes = assembly.GetCustomAttributes(typeof(T), false);
+            if (attributes.Length == 0)
+            {
+                return null;
+            }
+            return attributes.OfType<T>().SingleOrDefault();
+        }
 
         protected override object HandleQuestion(IThermometerQuestion question)
         {
-            return new {};
+            var componentAssembly = GetComponentAssembly();
+            var componentAssemblyName = componentAssembly.GetName();
+
+            return new
+            {
+                product = GetAssemblyAttribute<AssemblyProductAttribute>(componentAssembly).Product,
+                component = componentAssemblyName.Name,
+                productVersion = GetAssemblyAttribute<AssemblyFileVersionAttribute>(componentAssembly).Version,
+                buildId = GetAssemblyAttribute<AssemblyInformationalVersionAttribute>(componentAssembly).InformationalVersion,
+            };
         }
     }
 }
