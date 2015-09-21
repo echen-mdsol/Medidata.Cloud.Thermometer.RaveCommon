70e81f43a7f6174bbf776371c8e18b38c73b1333
Cheng Huang <chuang@mdsol.com>
2015-09-21 10:57:55 -0400

    1. Minor typo fix. 2. Change the deserialization way to use XPath.

--- Mct.RaveCommon/DBInfoHandler.cs
+++ Mct.RaveCommon/DBInfoHandler.cs
@@ -1,49 +1,72 @@
-﻿using System.Collections.Generic;
+﻿using System;
 using System.Configuration;
-using System.IO;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
-using System.Xml.Serialization;
-using Medidata.Cloud.Thermometer.RaveCommon.DBInfo;
+using System.Xml;
 
 namespace Medidata.Cloud.Thermometer.RaveCommon
 {
 	public class DBInfoHandler : ThermometerBaseHandler
 	{
-
 		protected override object HandleQuestion(IThermometerQuestion question)
 		{
 			var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
 			var dataSettingSection = config.Sections["DataSettings"];
 			if (dataSettingSection == null)
 			{
-				return new {Error = "DataSettings section cannot be found"};
+				return new {Error = "DataSettings section could not be found"};
 			}
 			var rawXml = dataSettingSection.SectionInformation.GetRawXml();
-			var serializer = new XmlSerializer(typeof(DataSettings));
-			using (TextReader reader = new StringReader(rawXml))
+			var xDoc = new XmlDocument();
+			xDoc.LoadXml(rawXml);
+			var timeOut = int.Parse(xDoc.SelectSingleNode("//DataSettings/Caching").Attributes["TimeOut"].Value);
+			var defaultHint = xDoc.SelectSingleNode("//DataSettings/DefaultHint").Attributes["Value"].Value;
+			var connList =
+				xDoc.SelectNodes("//DataSettings/ConnectionSettings/ConnectionSetting")
+					.OfType<XmlNode>()
+					.Select(GenerateConnectionStringObject)
+					.ToList();
+			return new {CachingTimeOut = timeOut, DefaultHint = defaultHint, ConnectionSettings = connList};
+		}
+
+		private object GenerateConnectionStringObject(XmlNode node)
+		{
+			var hint = node.Attributes["DataSourceHint"].Value;
+			var connString = node.Attributes["ConnectionString"].Value;
+			var providerType = node.Attributes["ProviderType"].Value;
+			var sb = new SqlConnectionStringBuilder(connString);
+			return new
 			{
-				var result = (DataSettings)serializer.Deserialize(reader);
-				result.ConnectionSettings.ConnectionSetting.ForEach(e => e.FillConnectionStringDetails());
-				return
-					new
+				DataSourceHint = hint,
+				ServerName = sb.DataSource,
+				DatabaseName = sb.InitialCatalog,
+				ProviderType = providerType,
+				sb.MaxPoolSize,
+				sb.MultipleActiveResultSets,
+				ConnectionTimeOut = sb.ConnectTimeout,
+				CanConnect = TryConnectionString(connString)
+			};
+		}
+
+		private object TryConnectionString(string connString)
+		{
+			using (var conn = new SqlConnection(connString))
+			{
+				try
+				{
+					conn.Open();
+					if (conn.State == ConnectionState.Open)
 					{
-						Caching = result.Caching,
-						DefaultHint = result.DefaultHint,
-						ConnectionSettings =
-							result.ConnectionSettings.ConnectionSetting.Select(
-								c =>
-									new
-									{
-										DataSourceHint = c.DataSourceHint,
-										ServerName = c.ServerName,
-										DatabaseName = c.DatabaseName,
-										ProviderType = c.ProviderType,
-										MaxPoolSize = c.MaxPoolSize,
-										MultipleActiveResultSets = c.MultipleActiveResultSets,
-										ConnectionTimeOut = c.ConnectionTimeOut
-									}).ToList()
-					};
-			}	
+						return "Yes";
+					}
+					return conn.State.ToString();
+				}
+				catch (Exception e)
+				{
+					return e;
+				}
+			}
 		}
 	}
 }
