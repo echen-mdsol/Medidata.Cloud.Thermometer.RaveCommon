44c23ceb7cb57a48083f37a01f4ba9d2e927841f
jshao <jshao@mdsol.com>
2015-10-14 10:20:59 -0400

    Fix disclosure of user id and password in connection string

--- Mct.RaveCommon/DynamicExtensions.cs
+++ Mct.RaveCommon/DynamicExtensions.cs
@@ -2,27 +2,24 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Dynamic;
 using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace Medidata.Cloud.Thermometer.RaveCommon
 {
     internal static class DynamicExtensions
     {
-        public static dynamic ToDynamic(this object target)
+        public static dynamic ToDynamicJson(this object target)
         {
             if (target == null) return null;
 
             var expandoObject = target as ExpandoObject;
             if (expandoObject != null) return expandoObject;
 
-            var expando = new ExpandoObject();
-            var dic = (IDictionary<string, object>) expando;
-            var properties = TypeDescriptor.GetProperties(target.GetType()).OfType<PropertyDescriptor>();
-            foreach (var property in properties)
-            {
-                dic.Add(property.Name, property.GetValue(target));
-            }
+            var json = JsonConvert.SerializeObject(target, new StringEnumConverter());
+            var obj = JsonConvert.DeserializeObject<ExpandoObject>(json,  new ExpandoObjectConverter());
 
-            return expando;
+            return obj;
         }
     }
 }
