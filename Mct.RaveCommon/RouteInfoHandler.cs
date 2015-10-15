9f6ed2c2bda81c308012e3f50dc2eb637b7f998b
jshao <jshao@mdsol.com>
2015-10-15 12:22:11 -0400

    Add RouteInfoHandler

---
+++ Mct.RaveCommon/RouteInfoHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using System.Web.Routing;
+
+namespace Medidata.Cloud.Thermometer.RaveCommon
+{
+    public class RouteInfoHandler : ThermometerBaseHandler
+    {
+        protected override object HandleQuestion(IThermometerQuestion question)
+        {
+            return RouteTable.Routes.OfType<Route>().Select(x => x.Url);
+        }
+    }
+}
